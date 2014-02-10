f4c697bcb9e623bc321ae719df023312a889d7ad
Desmazieres Jacques (DSE) <noreply@etat.ge.ch>
2014-02-10 14:05:15 +0000

    Correction display statistiques
    git-svn-id: http://scm.etat-ge.ch:21080/cti.composant/ct_referentiels-ofs/trunk@30940 222efd1c-12ae-ee84-b859-f398d68c9be8

--- referentiel-pays/src/main/java/ch/ge/cti/ct/referentiels/pays/service/jmx/DisplayMode.java
+++ referentiel-pays/src/main/java/ch/ge/cti/ct/referentiels/pays/service/jmx/DisplayMode.java
@@ -18,57 +18,57 @@ import ch.ge.cti.ct.referentiels.ofs.service.jmx.StatistiquesServiceSingleton;
  * 
  */
 public enum DisplayMode implements Renderable {
-	/** mode HTML: XML "escapé" */
-	HTML {
-		@Override
-		public String render() {
-			final String xml = XML.render();
-			return StringEscapeUtils.escapeHtml(xml);
-		}
-	},
-	/** XML */
-	XML {
-		@Override
-		public String render() {
-			final StringBuilder xml = new StringBuilder();
-			xml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-			xml.append("<referentiel-communes-statistiques>\n");
-			xml.append("  <cache>\n");
-			for (CacheEnum cache : CacheEnum.values()) {
-				CacheStatUtil.renderCacheStats(cache.name(),
-						CacheManager.instance.getCaches().get(cache.name()),
-						xml);
-			}
-			xml.append("  </cache>\n");
-			for (Entry<Call, Stat> entry : StatistiquesServiceSingleton.instance
-					.getStatistiques().entrySet()) {
-				if (entry.getKey().getParametre() != null) {
-					xml.append("  <call method=\"")
-							.append(entry.getKey().getMethode())
-							.append("\" parametre=\"")
-							.append(entry.getKey().getParametre())
-							.append("\" count=\"")
-							.append(entry.getValue().getCalls().longValue())
-							.append("\" total-laps-nano=\"")
-							.append(entry.getValue().getLaps().longValue())
-							.append("\"/>\n");
-				} else {
-					xml.append("  <call method=\"")
-							.append(entry.getKey().getMethode())
-							.append("\" count=\"")
-							.append(entry.getValue().getCalls().longValue())
-							.append("\" total-laps-nano=\"")
-							.append(entry.getValue().getLaps().longValue())
-							.append("\"/>\n");
-				}
-			}
-			xml.append("</referentiel-communes-statistiques>");
-			return xml.toString();
-		}
-	};
-
-	private enum CacheEnum {
-		cantons, districts, communes;
+    /** mode HTML: XML "escapé" */
+    HTML {
+	@Override
+	public String render() {
+	    final String xml = XML.render();
+	    return StringEscapeUtils.escapeHtml(xml);
 	}
+    },
+    /** XML */
+    XML {
+	@Override
+	public String render() {
+	    final StringBuilder xml = new StringBuilder();
+	    xml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+	    xml.append("<referentiel-pays-territoires-statistiques>\n");
+	    xml.append("  <cache>\n");
+	    for (CacheEnum cache : CacheEnum.values()) {
+		CacheStatUtil.renderCacheStats(cache.name(),
+			CacheManager.instance.getCaches().get(cache.name()),
+			xml);
+	    }
+	    xml.append("  </cache>\n");
+	    for (Entry<Call, Stat> entry : StatistiquesServiceSingleton.instance
+		    .getStatistiques().entrySet()) {
+		if (entry.getKey().getParametre() != null) {
+		    xml.append("  <call method=\"")
+			    .append(entry.getKey().getMethode())
+			    .append("\" parametre=\"")
+			    .append(entry.getKey().getParametre())
+			    .append("\" count=\"")
+			    .append(entry.getValue().getCalls().longValue())
+			    .append("\" total-laps-nano=\"")
+			    .append(entry.getValue().getLaps().longValue())
+			    .append("\"/>\n");
+		} else {
+		    xml.append("  <call method=\"")
+			    .append(entry.getKey().getMethode())
+			    .append("\" count=\"")
+			    .append(entry.getValue().getCalls().longValue())
+			    .append("\" total-laps-nano=\"")
+			    .append(entry.getValue().getLaps().longValue())
+			    .append("\"/>\n");
+		}
+	    }
+	    xml.append("</referentiel-pays-territoires-statistiques>");
+	    return xml.toString();
+	}
+    };
+
+    private enum CacheEnum {
+	cantons, districts, communes;
+    }
 
 }
