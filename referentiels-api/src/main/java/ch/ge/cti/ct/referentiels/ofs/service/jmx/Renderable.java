e83d72d2be041ff2666f0cf80314e9e88976652a
Desmazieres Jacques (DSE) <noreply@etat.ge.ch>
2014-03-14 14:07:52 +0000

    Sonar
    git-svn-id: http://scm.etat-ge.ch:21080/cti.composant/ct_referentiels-ofs/trunk@32284 222efd1c-12ae-ee84-b859-f398d68c9be8

--- referentiels-api/src/main/java/ch/ge/cti/ct/referentiels/ofs/service/jmx/Renderable.java
+++ referentiels-api/src/main/java/ch/ge/cti/ct/referentiels/ofs/service/jmx/Renderable.java
@@ -1,10 +1,17 @@
 package ch.ge.cti.ct.referentiels.ofs.service.jmx;
 
+/**
+ * Interface de classes pouvant être traitées pour le rendering (génération de
+ * rapport JMX)
+ * 
+ * @author DESMAZIERESJ
+ * 
+ */
 public interface Renderable {
-	/**
-	 * Définition de la méthode de rendering
-	 * 
-	 * @return données mises en forme
-	 */
-	String render();
+    /**
+     * Définition de la méthode de rendering
+     * 
+     * @return données mises en forme
+     */
+    String render();
 }
