772ca940dd65cb45592cf735caccbd893cdf6635
Desmazieres Jacques (DSE) <noreply@etat.ge.ch>
2014-04-08 06:25:47 +0000

    Séquence des initialisations
    git-svn-id: http://scm.etat-ge.ch:21080/cti.composant/ct_referentiels-ofs/trunk@32860 222efd1c-12ae-ee84-b859-f398d68c9be8

--- referentiels-api/src/main/java/ch/ge/cti/ct/referentiels/ofs/cache/CacheManager.java
+++ referentiels-api/src/main/java/ch/ge/cti/ct/referentiels/ofs/cache/CacheManager.java
@@ -1,5 +1,6 @@
 package ch.ge.cti.ct.referentiels.ofs.cache;
 
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -14,44 +15,47 @@ import com.google.common.cache.CacheBuilder;
  * 
  */
 public enum CacheManager {
-	/** instance du singleton */
-	instance;
+    /** instance du singleton */
+    instance;
 
-	/** liste des caches instanciés */
-	private final Map<String, Cache<String, ?>> caches = new Hashtable<String, Cache<String, ? extends Object>>();
+    /** liste des caches instanciés */
+    private final Map<String, Cache<String, ?>> caches = new Hashtable<String, Cache<String, ? extends Object>>();
 
-	/**
-	 * Recherche / instanciation d'une instance de cache
-	 * 
-	 * @param name nom du cache
-	 * @param stats activation des statistiques
-	 * @param size taille maximale du cache
-	 * @return cache
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public Cache<String, ? extends Object> getCache(final String name,
-			final boolean stats, final int size) {
-		if (caches.containsKey(name)) {
-			return caches.get(name);
-		}
-		final CacheBuilder builder = CacheBuilder.newBuilder().weakValues()
-				.maximumSize(size);
-		if (stats) {
-			builder.recordStats();
-		}
-		final Cache<String, ? extends Object> cache = builder.build();
-		caches.put(name, cache);
-		return cache;
+    /**
+     * Recherche / instanciation d'une instance de cache
+     * 
+     * @param name
+     *            nom du cache
+     * @param stats
+     *            activation des statistiques
+     * @param size
+     *            taille maximale du cache
+     * @return cache
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public Cache<String, ? extends Object> getCache(final String name,
+	    final boolean stats, final int size) {
+	if (caches.containsKey(name)) {
+	    return caches.get(name);
 	}
-
-	/**
-	 * Getter du cache<br/>
-	 * Exposé pour permettre une exploitation par le MBean
-	 * 
-	 * @return caches
-	 */
-	public Map<String, Cache<String, ?>> getCaches() {
-		return caches;
+	final CacheBuilder builder = CacheBuilder.newBuilder().weakValues()
+		.maximumSize(size);
+	if (stats) {
+	    builder.recordStats();
 	}
+	final Cache<String, ? extends Object> cache = builder.build();
+	caches.put(name, cache);
+	return cache;
+    }
+
+    /**
+     * Getter du cache<br/>
+     * Exposé pour permettre une exploitation par le MBean
+     * 
+     * @return caches
+     */
+    public Map<String, Cache<String, ?>> getCaches() {
+	return Collections.unmodifiableMap(caches);
+    }
 
 }
--- referentiels-api/src/test/java/ch/ge/cti/ct/referentiels/ofs/cache/CacheManagerTest.java
+++ referentiels-api/src/test/java/ch/ge/cti/ct/referentiels/ofs/cache/CacheManagerTest.java
@@ -9,30 +9,31 @@ import com.google.common.cache.Cache;
 
 public class CacheManagerTest {
 
-	@Test
-	public void testGetCache() {
-		final Cache<String, ? extends Object> cache1 = CacheManager.instance
-				.getCache("cache1", false, 100);
-		assertNotNull("Erreur d'instanciation du cache", cache1);
-	}
+    @Test
+    public void testGetCache() {
+	final Cache<String, ? extends Object> cache0 = CacheManager.instance
+		.getCache("cache0", false, 100);
+	assertNotNull("Erreur d'instanciation du cache", cache0);
+    }
 
-	@Test
-	public void testGetCaches() {
-		final Cache<String, ? extends Object> cache1 = CacheManager.instance
-				.getCache("cache1", false, 100);
-		assertNotNull("Erreur d'instanciation du cache", cache1);
-		assertEquals("Erreur de récupération des caches", 1,
-				CacheManager.instance.getCaches().size());
-		final Cache<String, ? extends Object> cache2 = CacheManager.instance
-				.getCache("cache2", false, 100);
-		assertNotNull("Erreur d'instanciation du cache", cache2);
-		assertEquals("Erreur de récupération des caches", 2,
-				CacheManager.instance.getCaches().size());
-		final Cache<String, ? extends Object> cache3 = CacheManager.instance
-				.getCache("cache2", false, 100);
-		assertNotNull("Erreur d'instanciation du cache", cache3);
-		assertEquals("Erreur de récupération des caches", 2,
-				CacheManager.instance.getCaches().size());
-	}
+    @Test
+    public void testGetCaches() {
+	final int initCache = CacheManager.instance.getCaches().size();
+	final Cache<String, ? extends Object> cache1 = CacheManager.instance
+		.getCache("cache1", false, 100);
+	assertNotNull("Erreur d'instanciation du cache", cache1);
+	assertEquals("Erreur de récupération des caches", initCache + 1,
+		CacheManager.instance.getCaches().size());
+	final Cache<String, ? extends Object> cache2 = CacheManager.instance
+		.getCache("cache2", false, 100);
+	assertNotNull("Erreur d'instanciation du cache", cache2);
+	assertEquals("Erreur de récupération des caches", initCache + 2,
+		CacheManager.instance.getCaches().size());
+	final Cache<String, ? extends Object> cache3 = CacheManager.instance
+		.getCache("cache2", false, 100);
+	assertNotNull("Erreur d'instanciation du cache", cache3);
+	assertEquals("Erreur de récupération des caches", initCache + 2,
+		CacheManager.instance.getCaches().size());
+    }
 
 }
