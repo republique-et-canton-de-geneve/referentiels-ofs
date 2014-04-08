1fe09225f7fac1206ad1f5dae6ac068068edb759
Desmazieres Jacques (DSE) <noreply@etat.ge.ch>
2014-04-08 06:31:55 +0000

    Intégration référentiel catégories socioprofessionnelles
    git-svn-id: http://scm.etat-ge.ch:21080/cti.composant/ct_referentiels-ofs/trunk@32864 222efd1c-12ae-ee84-b859-f398d68c9be8

---
+++ referentiel-integration-tests/src/test/java/ch/ge/cti/ct/referentiels/socioprofessionnel/interfaces/ws/ReferentielSocioprofessionnelClientTestIT.java
@@ -0,0 +1,43 @@
+package ch.ge.cti.ct.referentiels.socioprofessionnel.interfaces.ws;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.openejb.junit.ApplicationComposer;
+import org.apache.openejb.junit.EnableServices;
+import org.apache.openejb.junit.Module;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.ge.cti.ct.referentiels.AbstractClientTest;
+import ch.ge.cti.ct.referentiels.socioprofessionnel.client.ReferentielSocioprofessionnelClient;
+
+@EnableServices(value = "jax-ws")
+@RunWith(ApplicationComposer.class)
+public class ReferentielSocioprofessionnelClientTestIT extends
+	AbstractClientTest {
+
+    @Module
+    public static Class<?>[] communes() throws Exception {
+	return new Class<?>[] { ReferentielSocioprofessionnelSEI.class };
+    }
+
+    @Test
+    public void test() throws Exception {
+	final ReferentielSocioprofessionnelWS client = ReferentielSocioprofessionnelClient.Factory
+		.getClient("http://127.0.0.1:4204/socioprofessionnel/ReferentielSocioprofessionnelSEI?wsdl");
+
+	assertTrue(client.getNiveaux1().size() > 0);
+	assertTrue(client.searchNiveaux1("Dirigeant").size() > 0);
+	assertTrue(client.searchNiveaux1Regexp("^Dirigeant").size() > 0);
+
+	assertTrue(client.getNiveaux2().size() > 0);
+	assertTrue(client.getNiveaux2ByNiveau1(20).size() > 0);
+	assertTrue(client.searchNiveaux2("Dirigeant").size() > 0);
+	assertTrue(client.searchNiveaux2Regexp("^Dirigeant").size() > 0);
+
+	assertNotNull(client.getNiveau1(20));
+	assertNotNull(client.getNiveau2(200));
+	assertNotNull(client.getNiveaux2ByNiveau1(20));
+    }
+}
