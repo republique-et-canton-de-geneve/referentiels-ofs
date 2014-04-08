e7f6c5dd89d02cabe91389c9dd766f5ea6764b58
Desmazieres Jacques (DSE) <noreply@etat.ge.ch>
2014-04-08 06:11:02 +0000

    Séquence des initialisations
    git-svn-id: http://scm.etat-ge.ch:21080/cti.composant/ct_referentiels-ofs/trunk@32858 222efd1c-12ae-ee84-b859-f398d68c9be8

--- referentiels-api/src/test/java/ch/ge/cti/ct/referentiels/ofs/data/SDMXParserTest.java
+++ referentiels-api/src/test/java/ch/ge/cti/ct/referentiels/ofs/data/SDMXParserTest.java
@@ -11,33 +11,33 @@ import org.sdmxsource.sdmx.api.model.StructureWorkspace;
 
 public class SDMXParserTest extends AbstractDataTest {
 
-	private static SDMXParser parser;
+    private static SDMXParser parser;
 
-	@BeforeClass
-	public static void setupClass() throws Exception {
-		parser = ctxt.getBean(SDMXParser.class);
-		assertNotNull(parser);
-	}
+    @BeforeClass
+    public static void setupTestClass() throws Exception {
+	parser = ctxt.getBean(SDMXParser.class);
+	assertNotNull(parser);
+    }
 
-	@Test
-	public void testParse() throws Exception {
-		File file = new File("src/test/resources/test.xml");
-		StructureWorkspace sw = parser.parse(file.toURI().toURL());
-		assertNotNull("Le résultat du parsing est incorrect", sw);
-		assertEquals("Erreur de parsing du header",
-				"f198f7a6-57cd-4324-81a2-77b71e3edbef",
-				sw.getStructureBeans(false).getHeader().getId());
-		System.err.println(sw.getStructureBeans(false).getCodelists()
-				.iterator().next().getCodeById("02"));
-	}
+    @Test
+    public void testParse() throws Exception {
+	final File file = new File("src/test/resources/test.xml");
+	final StructureWorkspace sw = parser.parse(file.toURI().toURL());
+	assertNotNull("Le résultat du parsing est incorrect", sw);
+	assertEquals("Erreur de parsing du header",
+		"f198f7a6-57cd-4324-81a2-77b71e3edbef",
+		sw.getStructureBeans(false).getHeader().getId());
+	System.err.println(sw.getStructureBeans(false).getCodelists()
+		.iterator().next().getCodeById("02"));
+    }
 
-	@Test
-	public void testParseEncoding() throws Exception {
-		File file = new File("src/test/resources/test.xml");
-		StructureWorkspace sw = parser.parse(file.toURI().toURL());
-		assertEquals("Erreur de parsing des listes", "Société simple", sw
-				.getStructureBeans(false).getCodelists().iterator().next()
-				.getCodeById("02").getName());
-	}
+    @Test
+    public void testParseEncoding() throws Exception {
+	final File file = new File("src/test/resources/test.xml");
+	final StructureWorkspace sw = parser.parse(file.toURI().toURL());
+	assertEquals("Erreur de parsing des listes", "Société simple", sw
+		.getStructureBeans(false).getCodelists().iterator().next()
+		.getCodeById("02").getName());
+    }
 
 }
