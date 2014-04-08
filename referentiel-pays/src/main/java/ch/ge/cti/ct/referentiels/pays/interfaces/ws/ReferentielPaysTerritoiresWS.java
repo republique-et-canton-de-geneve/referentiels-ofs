97b174cb30ad778054b1ce54e84b64289934f0b7
Desmazieres Jacques (DSE) <noreply@etat.ge.ch>
2014-04-08 07:16:04 +0000

    sonar
    git-svn-id: http://scm.etat-ge.ch:21080/cti.composant/ct_referentiels-ofs/trunk@32867 222efd1c-12ae-ee84-b859-f398d68c9be8

--- referentiel-pays/src/main/java/ch/ge/cti/ct/referentiels/pays/interfaces/ws/ReferentielPaysTerritoiresWS.java
+++ referentiel-pays/src/main/java/ch/ge/cti/ct/referentiels/pays/interfaces/ws/ReferentielPaysTerritoiresWS.java
@@ -32,64 +32,61 @@ public interface ReferentielPaysTerritoiresWS {
 
     @WebMethod(operationName = "getContinents", action = "getContinents")
     @WebResult(name = "continent")
-    public List<ContinentWS> getContinents() throws ReferentielOfsException;
+    List<ContinentWS> getContinents() throws ReferentielOfsException;
 
     @WebMethod(operationName = "getContinent", action = "getContinent")
     @WebResult(name = "continent")
-    public ContinentWS getContinent(
-	    @WebParam(name = "continent") final int continentId)
+    ContinentWS getContinent(@WebParam(name = "continent") final int continentId)
 	    throws ReferentielOfsException;
 
     @WebMethod(operationName = "getRegionsByContinent", action = "getRegionsByContinent")
     @WebResult(name = "region")
-    public List<RegionWS> getRegionsByContinent(
+    List<RegionWS> getRegionsByContinent(
 	    @WebParam(name = "continent") final int continentId)
 	    throws ReferentielOfsException;
 
     @WebMethod(operationName = "getRegions", action = "getRegions")
     @WebResult(name = "region")
-    public List<RegionWS> getRegions() throws ReferentielOfsException;
+    List<RegionWS> getRegions() throws ReferentielOfsException;
 
     @WebMethod(operationName = "getRegion", action = "getRegion")
     @WebResult(name = "region")
-    public RegionWS getRegion(@WebParam(name = "region") final int regionId)
+    RegionWS getRegion(@WebParam(name = "region") final int regionId)
 	    throws ReferentielOfsException;
 
     @WebMethod(operationName = "getPaysByRegion", action = "getPaysByRegion")
     @WebResult(name = "pays")
-    public List<PaysWS> getPaysByRegion(
-	    @WebParam(name = "region") final int regionId)
+    List<PaysWS> getPaysByRegion(@WebParam(name = "region") final int regionId)
 	    throws ReferentielOfsException;
 
     @WebMethod(operationName = "getPaysByContinent", action = "getPaysByContinent")
     @WebResult(name = "pays")
-    public List<PaysWS> getPaysByContinent(
+    List<PaysWS> getPaysByContinent(
 	    @WebParam(name = "continent") final int continentId)
 	    throws ReferentielOfsException;
 
     @WebMethod(operationName = "getPays", action = "getPays")
     @WebResult(name = "pays")
-    public List<PaysWS> getPays() throws ReferentielOfsException;
+    List<PaysWS> getPays() throws ReferentielOfsException;
 
     @WebMethod(operationName = "getPaysByIso2", action = "getPaysByIso2")
     @WebResult(name = "pays")
-    public PaysWS getPaysByIso2(@WebParam(name = "iso2") final String iso2)
+    PaysWS getPaysByIso2(@WebParam(name = "iso2") final String iso2)
 	    throws ReferentielOfsException;
 
     @WebMethod(operationName = "getPaysByIso3", action = "getPaysByIso3")
     @WebResult(name = "pays")
-    public PaysWS getPaysByIso3(@WebParam(name = "iso3") final String iso3)
+    PaysWS getPaysByIso3(@WebParam(name = "iso3") final String iso3)
 	    throws ReferentielOfsException;
 
     @WebMethod(operationName = "searchPays", action = "searchPays")
     @WebResult(name = "pays")
-    public List<PaysWS> searchPays(
-	    @WebParam(name = "critere") final String critere)
+    List<PaysWS> searchPays(@WebParam(name = "critere") final String critere)
 	    throws ReferentielOfsException;
 
     @WebMethod(operationName = "searchPaysRegexp", action = "searchPaysRegexp")
     @WebResult(name = "pays")
-    public List<PaysWS> searchPaysRegexp(
+    List<PaysWS> searchPaysRegexp(
 	    @WebParam(name = "critere") final String critere)
 	    throws ReferentielOfsException;
 
