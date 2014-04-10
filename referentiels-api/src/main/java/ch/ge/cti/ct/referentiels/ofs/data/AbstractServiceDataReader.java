757cb1c7ceb58dda4292c89ac215d9db2ab13179
Desmazieres Jacques (DSE) <noreply@etat.ge.ch>
2014-04-10 05:36:21 +0000

    jenkins / sonar
    git-svn-id: http://scm.etat-ge.ch:21080/cti.composant/ct_referentiels-ofs/trunk@32930 222efd1c-12ae-ee84-b859-f398d68c9be8

--- referentiels-api/src/main/java/ch/ge/cti/ct/referentiels/ofs/data/AbstractServiceDataReader.java
+++ referentiels-api/src/main/java/ch/ge/cti/ct/referentiels/ofs/data/AbstractServiceDataReader.java
@@ -1,6 +1,8 @@
 package ch.ge.cti.ct.referentiels.ofs.data;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import ch.ge.cti.ct.act.configuration.DistributionFactory;
@@ -14,101 +16,129 @@ import ch.ge.cti.ct.referentiels.ofs.ReferentielOfsException;
  */
 public abstract class AbstractServiceDataReader<T> {
 
-	/** url du fichier xml */
-	private URL xmlFile = null;
+    /** url du fichier xml */
+    private URL xmlFile = null;
 
-	/**
-	 * retourne le nom de l'entrée de configuration de la localisation du
-	 * fichier sdmx<br/>
-	 * La clef de configuration doit être unique pour chaque référentiel
-	 * 
-	 * @return clef de configuration
-	 */
-	protected abstract String getConfigurationEntry();
+    /**
+     * retourne le nom de l'entrée de configuration de la localisation du
+     * fichier sdmx<br/>
+     * La clef de configuration doit être unique pour chaque référentiel
+     * 
+     * @return clef de configuration
+     */
+    protected abstract String getConfigurationEntry();
 
-	/**
-	 * retourne l'adapteur de données SDMX spécifique au fichier de données
-	 * 
-	 * @return SDMX data adaptor
-	 */
-	protected abstract AbstractSDMXDataAdaptor<T> getDataAdaptor();
+    /**
+     * retourne l'adapteur de données SDMX spécifique au fichier de données
+     * 
+     * @return SDMX data adaptor
+     */
+    protected abstract AbstractSDMXDataAdaptor<T> getDataAdaptor();
 
-	/**
-	 * Chargement du fichier référentiel<br/>
-	 * <ol>
-	 * <li>recherche du fichier référentiel</li>
-	 * <li>lecture du fichier</li>
-	 * <li>parsing du flux XML</li>
-	 * </ol>
-	 * 
-	 * @return ReferentielCommunes
-	 * @throws ReferentielOfsException erreur de chargement
-	 */
-	public T read() throws ReferentielOfsException {
-		return (T) parse(getXmlFile());
+    /**
+     * Chargement du fichier référentiel<br/>
+     * <ol>
+     * <li>recherche du fichier référentiel</li>
+     * <li>lecture du fichier</li>
+     * <li>parsing du flux XML</li>
+     * </ol>
+     * 
+     * @return ReferentielCommunes
+     * @throws ReferentielOfsException
+     *             erreur de chargement
+     */
+    public T read() throws ReferentielOfsException {
+	return parse(getXmlFile());
+    }
+
+    /**
+     * Lecture & parsing du fichier XML
+     * 
+     * @param urlXML
+     *            url du fichier à lire
+     * @return ReferentielCommunes référentiel
+     * @throws ReferentielOfsException
+     *             erreur de chargement
+     */
+    protected T parse(final URL urlXML) throws ReferentielOfsException {
+	return getDataAdaptor().parse(urlXML);
+    }
+
+    /**
+     * Chemin du fichier XML
+     * 
+     * @return url du fichier XML
+     * @throws ReferentielOfsException
+     *             erreur de traitement
+     */
+    private URL getURL() throws ReferentielOfsException {
+	try {
+	    final String filename = DistributionFactory.getConfiguration()
+		    .getString(getConfigurationEntry());
+	    if (filename == null) {
+		throw new ReferentielOfsException(
+			"Impossible de trouver l'entrée ["
+				+ getConfigurationEntry()
+				+ "] dans le fichier Distribution.properties");
+	    }
+	    return toURL(filename);
+	} catch (final IOException ex) {
+	    throw new ReferentielOfsException(
+		    "Impossible de lire le fichier Distribution.properties", ex);
+	}
+    }
+
+    /**
+     * Convertion en URL supportant le format avec ou sans protocole
+     * 
+     * @param filename
+     *            nom du fichier
+     * @return url
+     * @throws ReferentielOfsException
+     *             erreur de convertion
+     */
+    private URL toURL(final String filename) throws ReferentielOfsException {
+	try {
+	    return new URL(filename);
+	} catch (final MalformedURLException mue) {
+	    try {
+		return new File(filename).toURI().toURL();
+	    } catch (final MalformedURLException mue2) {
+		throw new ReferentielOfsException("La valeur de l'entrée ["
+			+ getConfigurationEntry() + "] est invalide: "
+			+ filename, mue);
+	    }
+	}
+    }
+
+    /**
+     * getter url du fichier référentiel SDMX
+     * 
+     * @return url du fichier référentiel
+     * @throws ReferentielOfsException
+     *             erreur de traitement
+     */
+    public synchronized URL getXmlFile() throws ReferentielOfsException {
+	if (xmlFile == null) {
+	    xmlFile = getURL();
+	}
+	return xmlFile;
+    }
+
+    /** enumération des données de configuration */
+    public static enum Config {
+	/** variable d'environnement distribution.properties */
+	ENV("distribution.properties"),
+	/** chemin par défaut vers le fichier Distribution.properties */
+	FILE("/Distribution.properties");
+	private final String key;
+
+	private Config(final String key) {
+	    this.key = key;
 	}
 
-	/**
-	 * Lecture & parsing du fichier XML
-	 * 
-	 * @param urlXML url du fichier à lire
-	 * @return ReferentielCommunes référentiel
-	 * @throws ReferentielOfsException erreur de chargement
-	 */
-	protected T parse(final URL urlXML) throws ReferentielOfsException {
-		return getDataAdaptor().parse(urlXML);
+	public String key() {
+	    return this.key;
 	}
-
-	/**
-	 * Chemin du fichier XML
-	 * 
-	 * @return url du fichier XML
-	 * @throws ReferentielOfsException erreur de traitement
-	 */
-	private URL getURL() throws ReferentielOfsException {
-		try {
-			final String filename = DistributionFactory.getConfiguration()
-					.getString(getConfigurationEntry());
-			if (filename == null) {
-				throw new ReferentielOfsException(
-						"Impossible de trouver l'entrée ["
-								+ getConfigurationEntry()
-								+ "] dans le fichier Distribution.properties");
-			}
-			return new URL(filename);
-		} catch (IOException ex) {
-			throw new ReferentielOfsException(
-					"Impossible de lire le fichier Distribution.properties", ex);
-		}
-	}
-
-	/**
-	 * getter url du fichier référentiel SDMX
-	 * 
-	 * @return url du fichier référentiel
-	 * @throws ReferentielOfsException erreur de traitement
-	 */
-	public synchronized URL getXmlFile() throws ReferentielOfsException {
-		if (xmlFile == null) {
-			xmlFile = getURL();
-		}
-		return xmlFile;
-	}
-
-	/** enumération des données de configuration */
-	public static enum Config {
-		/** variable d'environnement distribution.properties */
-		ENV("distribution.properties"),
-		/** chemin par défaut vers le fichier Distribution.properties */
-		FILE("/Distribution.properties");
-		private final String key;
-
-		private Config(final String key) {
-			this.key = key;
-		}
-
-		public String key() {
-			return this.key;
-		}
-	}
+    }
 }
