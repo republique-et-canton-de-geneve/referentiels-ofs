cc26c9f47c31bfc06c70a56a88a9de9c709b8af1
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2023-02-17 13:07:37 +0100

    OPENSRC-184 Éviter la destruction automatique des projets dans SonarCloud

--- referentiels-api/src/main/java/ch/ge/cti/ct/referentiels/ofs/Loggable.java
+++ referentiels-api/src/main/java/ch/ge/cti/ct/referentiels/ofs/Loggable.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package ch.ge.cti.ct.referentiels.ofs;
+package ch.ge.cti.ct.referentiels.ofs; 
 
 import org.slf4j.Logger;
 
